bceabbe8bc618e8db189e22978a48ea80ffeb180
Jason Tan <37030567+AeGGIndie@users.noreply.github.com>
2024-05-04 17:49:03 -0400

    feat(FAB): added basic fab

--- components/FAB.js
+++ components/FAB.js
@@ -1,23 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-
 export default function FAB({ className, children, position, onClick }) {
-  // Initialize theme based on localStorage or default to 'light'
-  const [theme, setTheme] = useState(
-    () => localStorage.getItem("theme") || "light"
-  );
-
-  // Apply the theme to the body or root element
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
-
   const getPosition = () => {
     switch (position) {
       case "topLeft":
