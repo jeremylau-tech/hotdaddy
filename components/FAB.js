ce891ac3995091550ac80e7b75de853ed4dd7559
TonyTran03 <tony.tran03@hotmail.com>
2024-05-05 04:43:08 -0400

    changes in toggle

--- components/FAB.js
+++ components/FAB.js
@@ -17,6 +17,22 @@ export default function FAB({ className, children, position, onClick }) {
   };
 
   return (
+    <div className={`absolute ${getPosition()}`}>
+      <button
+        onClick={onClick}
+        className={`btn btn-accent rounded-3xl ${className || ""}`}
+      >
+        {children}
+      </button>
+      <label className="flex cursor-pointer gap-2 ml-4 items-center ">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+        <input type="checkbox" checked={theme === 'dark'} onChange={toggleTheme} className="toggle theme-controller"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+      </label>
+
+
+
+    </div>
     <button
       onClick={onClick}
       className={`btn btn-accent rounded-3xl ${
